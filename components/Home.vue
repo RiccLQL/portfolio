02099e3ccaa8239e079fee3510d3bbaeea2d88d1
Ricky Liu <li.qi.liu@mail.mcgill.ca>
2021-03-18 02:54:52 -0400

    home page 50%

---
+++ components/Home.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="home">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"><path fill-opacity="1" d="M0,32L48,53.3C96,75,192,117,288,122.7C384,128,480,96,576,117.3C672,139,768,213,864,208C960,203,1056,117,1152,106.7C1248,96,1344,160,1392,192L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        <div class="home-content">
+            <transition name="slide-fade" mode="out-in">
+            <h2 class="main-page-name" :key="category">
+                <b>Ricky Liu</b> | {{category}}
+            </h2>
+            </transition>
+            <div class="subtitles">
+                <h4 class="subtitle">
+                    My Work
+                </h4>
+                <h4 class="subtitle">
+                    About
+                </h4>
+                <h4 class="subtitle">
+                    Contact
+                </h4>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data () {
+        return {
+            category: "Software developer",
+            categories: ["Software developer", "Entrepreneur", "Artist", "Community leader", "Map enjoyer"],
+            iterator: 1,
+        }
+    },
+    methods: {
+
+    },
+    mounted () {
+        setInterval(() => {
+            if (this.iterator >= this.categories.length) {
+                this.iterator = 0;
+            }
+            this.category = this.categories[this.iterator];
+            this.iterator++;
+        }, 3000)
+    }
+}
+</script>
+
+<style lang="scss">
+
+
+.home {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;    
+    display: flex;
+    flex-direction: column;    
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(209, 222, 248);
+}
+
+.home-background-img {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+}
+
+.home-content {
+    color: rgb(31, 26, 119);
+    font-family: 'Poppins';
+}
+
+.main-page-name {
+    animation-name: fade-in;
+    transition: all 1s;
+	animation-fill-mode: both;
+	animation-duration: 0.8s;
+}
+
+.main-page-title {
+    animation-name: fade-in;
+    transition: all 1s;
+	animation-fill-mode: both;
+	animation-duration: 0.8s;
+}
+
+.wave {
+    fill: rgb(181, 199, 237);
+    position: absolute;
+    bottom: 0;
+}
+
+.subtitles {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 2em;
+}
+
+.subtitle {
+    margin-left: 2em;
+    margin-right: 2em;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 1em;
+    padding-right: 1em;
+    border-radius: 2.2em;
+    background-color: rgb(181, 199, 237);
+}
+
+.subtitles .subtitle {
+    animation-name: fade-in;
+	animation-fill-mode: both;
+	animation-duration: 0.8s;
+}
+
+@keyframes fade-in {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+$animationDelay: 1;
+@for $i from 1 through 7 {
+	.subtitles .subtitle:nth-of-type(#{$i}) {
+		animation-delay: #{0.3+($i)/10}s;
+  }
+}
+
+.slide-fade-enter-active {
+  transition: all 1.1s ease;
+}
+.slide-fade-leave-active {
+  transition: all 1.1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+    <div>
+      <Home/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from '../components/Home';
+
+export default {
+
+  components: {
+    Home
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+body::-webkit-scrollbar {
+  display: none;
+}
+
+body {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+
+h2 {
+  font-size: 350%;
+  font-weight: 500;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 55px;
+  color: #35495e;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 25px 0;
+}
+
+.subtitle a {
+  font-weight: 500;
+  color: inherit;
+}
+
+.links {
+  padding-top: 15px;
+  margin-bottom: 20px;
+}
+
+.content-logos {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+}
+
+.plus {
+  font-size: 2.5rem;
+  margin: 15px;
+  color: #35495e;
+}
+
+.h3 {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-weight: 400;
+  margin: 10px;
+}
+</style>
